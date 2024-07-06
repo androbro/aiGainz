1e033d8550245ad321e7964fcdfe59fd32749627
koen de vulder <koen.de.vulder@tabaknatie.be>
2024-07-06 14:45:24 +0200

    feat: added navbar and state

---
+++ app/reactQueryProvider.tsx
@@ -0,0 +1,37 @@
+﻿import React, { useRef } from 'react';
+import { MutationCache, QueryCache, QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Toast } from 'primereact/toast';
+
+export function ReactQueryProvider({ children }: React.PropsWithChildren) {
+    const toast = useRef<Toast>(null);
+    const queryClient = new QueryClient({
+        queryCache: new QueryCache({
+            onError: (error, query) => {
+                // 🎉 only show error toasts if we already have data in the cache
+                // which indicates a failed background update
+                console.table(JSON.stringify(error));
+                // if (query.state.data !== undefined) {
+                //     console.table(JSON.stringify(error));
+                // }
+            },
+        }),
+        mutationCache: new MutationCache({
+            onError: (error, query) => {
+                // 🎉 only show error toasts if we already have data in the cache
+                // which indicates a failed background update
+                console.table(JSON.stringify(error));
+                // if (query.state.data !== undefined) {
+                //     console.table(JSON.stringify(error));
+                // }
+            },
+        }),
+    });
+
+    return (
+        <QueryClientProvider client={queryClient}>
+            <Toast ref={toast} />
+            {children}
+            {/*<ReactQueryDevtools initialIsOpen={false} />*/}
+        </QueryClientProvider>
+    );
+}
