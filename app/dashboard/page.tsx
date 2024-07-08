ec2ef077cd30a5451d18ec41d31a7b3390d78104
koen de vulder <koen.de.vulder@tabaknatie.be>
2024-07-08 14:27:37 +0200

    Revert "feat: dynamic data"
    This reverts commit 8f7f98058331e0edaf5040b209f1531333530133.

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,27 +1,19 @@
 ﻿'use client';
-import {useLoaderStore} from '@/app/store/loaderStore';
-import {useLoading} from '@/hooks/useLoading';
-import SmallStatsCard, {smallStatsCardProps} from '@/app/components/cards/smallStatsCard';
+import { useLoaderStore } from '@/app/store/loaderStore';
+import { useLoading } from '@/hooks/useLoading';
+import SmallStatsCard, { smallStatsCardProps } from '@/app/components/cards/smallStatsCard';
 import React from 'react';
 
-interface PageProps {
-}
+interface PageProps {}
 
 export default function Dashboard({}: PageProps) {
-    const {setIsLoading} = useLoaderStore();
+    const { setIsLoading } = useLoaderStore();
     const smallCardData: smallStatsCardProps[] = [
         {
             title: 'Overall Strength',
             data: {
                 prevScore: 10,
                 currentScore: 2,
-            },
-            charts: {
-                label: 'First Dataset',
-                data: [65, 59, 80, 81, 56, 55, 40],
-                fill: false,
-                borderColor: '--blue-500',
-                tension: 0.4,
             },
         },
         {
@@ -30,26 +22,12 @@ export default function Dashboard({}: PageProps) {
                 prevScore: 10,
                 currentScore: 44,
             },
-            charts: {
-                label: 'First Dataset',
-                data: [65, 59, 80, 81, 56, 55, 40],
-                fill: false,
-                borderColor: '--blue-500',
-                tension: 0.4,
-            },
         },
         {
             title: 'Nutrition Consistency',
             data: {
                 prevScore: 10,
                 currentScore: 41,
-            },
-            charts: {
-                label: 'First Dataset',
-                data: [65, 59, 80, 81, 56, 55, 40],
-                fill: false,
-                borderColor: '--blue-500',
-                tension: 0.4,
             },
         },
         {
@@ -58,27 +36,19 @@ export default function Dashboard({}: PageProps) {
                 prevScore: 10,
                 currentScore: 4,
             },
-            charts: {
-                label: 'First Dataset',
-                data: [65, 59, 80, 81, 56, 55, 40],
-                fill: false,
-                borderColor: '--blue-500',
-                tension: 0.4,
-            },
         },
     ];
     useLoading(false, setIsLoading);
     return (
         <>
-            <div
-                className="layout-container layout-light layout-colorscheme-menu layout-static layout-static-inactive p-ripple-disabled">
+            <div className="layout-container layout-light layout-colorscheme-menu layout-static layout-static-inactive p-ripple-disabled">
                 {/*top 4 stats cards*/}
                 <div className="flex flex-wrap justify-content-center">
                     {/*here 4 small cards will be aligned next to each other on a fullhd screen*/}
                     {smallCardData.map((card, index) => {
                         return (
                             <div key={index} className="col-3 col-md-3">
-                                <SmallStatsCard title={card.title} data={card.data} charts={card.charts}/>
+                                <SmallStatsCard title={card.title} data={card.data} />
                             </div>
                         );
                     })}
