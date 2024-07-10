d14dacecc99bdd9f6701c51b10acc023830f1e30
koen de vulder <devulderk@gmail.com>
2024-07-10 21:49:25 +0200

    clean

--- app/page.tsx
+++ app/page.tsx
@@ -9,17 +9,6 @@ export default function Home() {
         <div className="m-4">
             <div className="">
                 <div>
-                    {user ? (
-                        <div>
-                            <a href="/api/auth/logout">Logout</a>
-                            <h1>Welcome {user.name}!</h1>
-                            <p>Your email is {user.email}.</p>
-                        </div>
-                    ) : (
-                        <div>
-                            <h1>Welcome!</h1>
-                        </div>
-                    )}
                     <Dashboard />
                 </div>
             </div>
