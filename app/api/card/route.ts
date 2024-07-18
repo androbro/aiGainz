c92ee1d883dc050b10089bf1a52dfc12eb17897a
koen de vulder <devulderk@gmail.com>
2024-07-18 19:50:39 +0200

    fix: build

--- app/api/card/route.ts
+++ app/api/card/route.ts
@@ -120,6 +120,7 @@ export async function POST(request: NextRequest) {
                         dataPoints: {
                             create: dataPoints,
                         },
+                        dataType: dataSourceType,
                     },
                 },
             },
