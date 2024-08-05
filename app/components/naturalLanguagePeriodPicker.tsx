1f28764df34f79cf6e0b354bb7a1f2b2f269c51e
koen de vulder <devulderk@gmail.com>
2024-08-05 19:06:22 +0200

    feat: cleaned main and also data routing for date picker

--- app/components/naturalLanguagePeriodPicker.tsx
+++ app/components/naturalLanguagePeriodPicker.tsx
@@ -1,15 +1,20 @@
 import React, { useState } from 'react';
 import { InputText } from 'primereact/inputtext';
 import { parse } from 'chrono-node';
+import { Button } from 'primereact/button';
 
 interface NaturalLanguagePeriodPickerProps {
+    initialDate?: Date;
     onChange: (date: Date | null) => void;
+    onSearch?: (date: Date) => void;
 }
 
 export const NaturalLanguagePeriodPicker: React.FC<NaturalLanguagePeriodPickerProps> = ({
+    initialDate,
     onChange,
+    onSearch,
 }) => {
-    const [inputValue, setInputValue] = useState('');
+    const [inputValue, setInputValue] = useState<string | undefined>(initialDate?.toString());
 
     const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const value = e.target.value;
@@ -26,10 +31,16 @@ export const NaturalLanguagePeriodPicker: React.FC<NaturalLanguagePeriodPickerPr
     };
 
     return (
-        <InputText
-            value={inputValue}
-            onChange={handleInputChange}
-            placeholder="Enter a date (e.g., '7 months ago', 'last Friday')"
-        />
+        <div className="p-inputgroup flex-1">
+            <InputText
+                className="p-inputtext-sm"
+                value={inputValue}
+                onChange={handleInputChange}
+                placeholder="Enter a date (e.g., '7 months ago', 'last Friday')"
+                tooltip={'Enter a date (e.g., "7 months ago", "last Friday")'}
+                tooltipOptions={{ position: 'bottom' }}
+            />
+            <Button icon="pi pi-search" onClick={() => onSearch} />
+        </div>
     );
 };
--- app/home/components/mainContent.tsx
+++ app/home/components/mainContent.tsx
@@ -27,6 +27,10 @@ export default function MainContent() {
         setCards((prevCards) => prevCards.filter((card) => card.id !== id));
     };
 
+    const updateCardsWithNewPeriod = (date: Date) => {
+        //update cards with new period
+    };
+
     const renderCard = (card: ExtendedCard) => (
         <div className="col-12 md:col-12 lg:col-4" key={card.id}>
             <ChartCard card={card} onDelete={handleDeleteCard} />
@@ -34,14 +38,24 @@ export default function MainContent() {
     );
 
     return (
-        <div className="flex flex-column w-full surface-0 border-round-3xl ">
-            <div className="flex gap-3 m-4 p-2 mb-0 pb-0">
-                <div className="text-2xl" style={{ fontWeight: 500 }}>
-                    Home
-                </div>
-                <div>
-                    {pickedDate && pickedDate.toDateString()}
-                    <NaturalLanguagePeriodPicker onChange={setPickedDate} />
+        <div className="flex flex-column w-full surface-0 border-round-3xl">
+            <div className="flex align-items-center m-4 p-2 gap-4">
+                <div className="text-2xl font-medium">Home</div>
+                <div className="flex align-items-center gap-3">
+                    <NaturalLanguagePeriodPicker
+                        onChange={setPickedDate}
+                        initialDate={pickedDate!}
+                        onSearch={updateCardsWithNewPeriod}
+                    />
+                    {pickedDate && (
+                        <span className="text-sm">
+                            {pickedDate.toLocaleDateString('en-US', {
+                                year: 'numeric',
+                                month: 'long',
+                                day: 'numeric',
+                            })}
+                        </span>
+                    )}
                 </div>
             </div>
             <section className="flex-column w-full" style={{ minHeight: '400px' }}>
